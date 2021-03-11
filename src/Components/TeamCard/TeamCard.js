258dfd90be0b9246bd327cbfc2da13490bd76d79
bdrkreza <diureza@gamil.com>
2021-03-11 16:07:11 +0600

    update

--- src/Components/TeamCard/TeamCard.js
+++ src/Components/TeamCard/TeamCard.js
@@ -10,7 +10,7 @@ const TeamCard = () => {
         fetch(url)
             .then(response => response.json())
             .then(data => setTeam(data.teams))
-    }, [])
+    }, [teamLeague])
     return (
         <>
 

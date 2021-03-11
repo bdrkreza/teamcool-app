e46b4bfaacff11508055157fb12d77cf73bb9ee9
bdrkreza <diureza@gamil.com>
2021-03-11 15:26:25 +0600

    my project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./Components/About/About";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import NoMatch from "./Components/NoMatch/NoMatch";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Contact from "./Components/Contact/Contact";
+import TeamCard from "./Components/TeamCard/TeamCard";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/team/:teamLeague">
+            <TeamCard />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Players/Players.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Players = () => {
+    return (
+        <>
+            <div className="container-fluid nav_bg">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <h1>this is a Players</h1>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Players;
